dd3c51b96e279fbdd0bf6dcac2ee2c126353fb6e
Savannah Romberg <savannah.romberg@gmail.com>
2022-08-22 13:20:16 -0600

    passed tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const regex = /[^A-Za-z]/g
+
+const palindromes = function (string) {
+    let alphaString = string.replace(regex, "").toLowerCase();
+    let alphaArray = alphaString.split('');
+    let array = alphaArray.join('');
+    let revArray = alphaArray.reverse().join('');
+
+console.log(array, revArray);
+
+for (let i = 0; i <= array.length; i++) {
+    if (array === revArray) {
+        return true;
+    } else {
+        return false;
+    };
+};
 
 };
 
