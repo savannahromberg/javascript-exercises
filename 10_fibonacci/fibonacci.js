dfb6cb9a78d3dc7223559a176dfb1ffd0bf1a275
Savannah Romberg <savannah.romberg@gmail.com>
2022-08-22 13:17:57 -0600

    passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    parseInt(n);
 
+    if(n <= 0) {
+        return "OOPS";
+    };
+    
+    const result = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        result[i] = result[i - 2] + result[i - 1];
+    }
+
+    return result[result.length - 1];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
