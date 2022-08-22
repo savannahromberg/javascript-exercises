6fdd4237eaa605d9c7ec75029ab40572214138f1
Savannah Romberg <savannah.romberg@gmail.com>
2022-08-22 13:19:49 -0600

    passed tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let answer = a + b;
+  return answer;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let answer = a - b;
+  return answer;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let answer = 0;
+    for (let i = 0; i <= array.length; i++) {
+    if(typeof array[i] === `number`) answer += array[i];
+    }
+    return answer;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let answer = array.reduce((a, b) => a * b);
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let answer = Math.pow(a, b);
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let answer = 1;
+  if (n == 0 || n == 1){
+    return answer;
+  }else{
+    for(var i = n; i >= 1; i--){
+      answer = answer * i;
+    }
+    return answer;
+  }  
 };
 
 // Do not edit below this line
